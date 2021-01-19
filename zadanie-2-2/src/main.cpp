d4b2c4b480001b10b69222dc0c3f9500e8f8a71d
Karol Lechański <karol.lechanskio@student.wat.edu.pl>
2021-01-19 09:30:32 +0100

    Wykonano zadanie 2.2.1b

--- zadanie-2-2/src/main.cpp
+++ zadanie-2-2/src/main.cpp
@@ -9,5 +9,7 @@ digitalWrite (LED_BUILTIN, LOW );
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite (LED_BUILTIN, HIGH);
+  delay(500);
   digitalWrite (LED_BUILTIN, LOW);
+  delay(500);
 }
